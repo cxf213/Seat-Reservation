a08ee6d65990af53d100e779428c562f57244daf
MisakiAkeno <cxf213@outlook.com>
2023-05-14 16:38:48 +0800

    加入了预约界面的adapter

---
+++ app/src/main/java/com/cxfwork/libraryappointment/client/ReserveBtnAdapter.java
@@ -0,0 +1,55 @@
+package com.cxfwork.libraryappointment.client;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cxfwork.libraryappointment.R;
+
+import java.util.List;
+
+public class ReserveBtnAdapter extends RecyclerView.Adapter<ReserveBtnAdapter.ViewHolder> {
+    private List<Integer> buttonNumbers;
+
+    public ReserveBtnAdapter(List<Integer> buttonNumbers) {
+        this.buttonNumbers = buttonNumbers;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_reserve_button, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        int number = buttonNumbers.get(position);
+        if (position % 2 == 0) {
+            // 设置偶数位置的按钮为红色背景
+            holder.button.setBackgroundColor(ContextCompat.getColor(holder.itemView.getContext(), R.color.md_theme_light_primary));
+        } else {
+            // 设置奇数位置的按钮为蓝色背景
+            holder.button.setBackgroundColor(ContextCompat.getColor(holder.itemView.getContext(), R.color.md_theme_light_secondary));
+        }
+        holder.button.setText(String.valueOf(number));
+    }
+
+    @Override
+    public int getItemCount() {
+        return buttonNumbers.size();
+    }
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public Button button;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            button = itemView.findViewById(R.id.button);
+        }
+    }
+}
--- app/src/main/java/com/cxfwork/libraryappointment/ui/quickreservation/QuickReservationFragment.java
+++ app/src/main/java/com/cxfwork/libraryappointment/ui/quickreservation/QuickReservationFragment.java
@@ -9,12 +9,20 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.cxfwork.libraryappointment.client.ReserveBtnAdapter;
 import com.cxfwork.libraryappointment.databinding.FragmentQuickReservationBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuickReservationFragment extends Fragment {
 
     private FragmentQuickReservationBinding binding;
+    private ReserveBtnAdapter adapter;
+    private RecyclerView recyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,7 +31,26 @@ public class QuickReservationFragment extends Fragment {
 
         binding = FragmentQuickReservationBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+
+        recyclerView = binding.recyclerview;
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 4));
+
+        List<Integer> buttonNumbers = generateButtonNumbers(); // 生成按钮编号的数据源
+
+        adapter = new ReserveBtnAdapter(buttonNumbers);
+        recyclerView.setAdapter(adapter);
         return root;
+    }
+
+    private List<Integer> generateButtonNumbers() {
+        List<Integer> buttonNumbers = new ArrayList<>();
+        // 在这里生成按钮的编号，并添加到 buttonNumbers 列表中
+        for(int i = 0;i<200;i++){
+            buttonNumbers.add(i);
+        }
+
+        return buttonNumbers;
     }
 
     @Override
